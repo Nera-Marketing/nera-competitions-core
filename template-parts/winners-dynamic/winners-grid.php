5ef595043a6d1d0a1671294914babd7ca168591a
Minh Le <minh@neramarketing.co.uk>
2026-04-20 18:48:12 +0700

    dynamic winers

--- template-parts/winners-dynamic/winners-grid.php
+++ template-parts/winners-dynamic/winners-grid.php
@@ -247,7 +247,7 @@ $x_data_expr = 'neraWinnersDynamicGrid(' . $config_json . ')';
       <div
         x-ref="grid"
         x-show="showingCount > 0 || loading"
-        class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
+        class="grid grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-6"
       >
         <?php
         foreach ($rows as $row) {
