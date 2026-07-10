7416ee41e620131abefb5049a3e4ae111b636175
nera-Tzenmuster <t@neramarketing.co.uk>
2026-07-10 15:08:12 +0700

    Release v1.3.9

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'nera/competitions-standard',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'f6385c005b5a0dccd0bcd85aceb3c295b247ef09',
+        'reference' => '7d55bf4fa460a1554404937f137533120d27ee40',
         'type' => 'wordpress-theme',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'nera/competitions-standard' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'f6385c005b5a0dccd0bcd85aceb3c295b247ef09',
+            'reference' => '7d55bf4fa460a1554404937f137533120d27ee40',
             'type' => 'wordpress-theme',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
